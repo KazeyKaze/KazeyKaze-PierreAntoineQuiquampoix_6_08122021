7a0a54300c5385cc4db11d218d2518525e50b39d
Pierre-Antoine Quiquampoix <p.a.q@hotmail.fr>
2021-12-29 18:08:14 +0100

    Prise en main de React et affichage du logo - [Repose en paix Pandora car "Où que tu sois, où que tu ailles, je t'aime et tu fais partie de moi"]

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,5 +1,15 @@
+import Banner from "./Banner";
+import logo from "../assets/icon-left-font.png";
+import "../styles/App.css";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Banner>
+        <img src={logo} className="g-banner" alt="Logo Groupomania" />
+      </Banner>
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/Banner.js
@@ -0,0 +1,7 @@
+import "../styles/Banner.css";
+
+function Banner({ children }) {
+  return <div className="g-div-banner">{children}</div>;
+}
+
+export default Banner;
