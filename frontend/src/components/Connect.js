0591afbf7c2b902aa8addd79f37539e3728cba26
Pierre-Antoine Quiquampoix <p.a.q@hotmail.fr>
2022-01-03 18:51:53 +0100

    Premiers tests de connexion... Dans la douleur...

--- frontend/src/components/Connect.js
+++ frontend/src/components/Connect.js
@@ -1,35 +1,52 @@
 import "../styles/Connect.css";
 
+////////// LOGIQUE
+function connection() {
+  fetch("http://localhost:3000/api/auth/login", {
+    method: "POST",
+    body: JSON.stringify({
+      email: document.getElementById("login-email").value,
+      password: document.getElementById("login-password").value,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
+
+////////// STRUCTURE
 function Connect() {
   return (
     <div className="g-div-connect">
       {/* LOGIN */}
       <div className="g-div-login">
-        <form action="http://localhost:3000/api/auth/login" method="post">
+        <form action="">
           <div>
             <div className="g-div-h2">
-              <h2 >Connexion</h2>
+              <h2>Connexion</h2>
             </div>
             <label htmlFor="email">
               Email:
               <br />
             </label>
-            <input type="email" id="login-email" name="user_email" />
+            <input type="email" id="login-email" name="user_login_email" />
           </div>
           <div>
             <label htmlFor="password">
               Mot de passe:
               <br />
             </label>
-            <input type="text" id="login-password" name="user_password" />
+            <input type="text" id="login-password" name="user_login_password" />
           </div>
         </form>
-        <button>Connexion</button>
+        <button id="button-connect" onClick={connection}>
+          Connexion
+        </button>
       </div>
 
       {/* SIGNUP */}
       <div className="g-div-signup">
-        <form action="http://localhost:3000/api/auth/signup" method="post">
+        <form action="">
           <div>
             <div className="g-div-h2">
               <h2>Inscription</h2>
@@ -38,28 +55,40 @@ function Connect() {
               Email:
               <br />
             </label>
-            <input type="email" id="signup-email" name="user_email" />
+            <input type="email" id="signup-email" name="user_signup_email" />
           </div>
           <div>
             <label htmlFor="password">
               Mot de passe:
               <br />
             </label>
-            <input type="text" id="signup-password" name="user_password" />
+            <input
+              type="text"
+              id="signup-password"
+              name="user_signup_password"
+            />
           </div>
           <div>
             <label htmlFor="firstname">
               Prénom:
               <br />
             </label>
-            <input type="text" id="signup-firstname" name="user_firstname" />
+            <input
+              type="text"
+              id="signup-firstname"
+              name="user_signup_firstname"
+            />
           </div>
           <div>
             <label htmlFor="lastname">
               Nom:
               <br />
             </label>
-            <input type="text" id="signup-lastname" name="user_lastname" />
+            <input
+              type="text"
+              id="signup-lastname"
+              name="user_signup_lastname"
+            />
           </div>
         </form>
         <button>Inscription</button>
