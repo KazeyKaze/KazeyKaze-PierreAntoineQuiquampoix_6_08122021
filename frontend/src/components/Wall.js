e962c7311ef4b37504c5f1f6295d7afd13f4c83d
Pierre-Antoine Quiquampoix <p.a.q@hotmail.fr>
2022-01-05 18:40:22 +0100

    Récupération des posts fonctionelle... Y'a encore BEAUCOUP à faire... (les trier par ordre chronologique + afficher le nom et le prénom de l'auteur + afficher la date/heure du post + tout le CRUD + le style)

--- frontend/src/components/Wall.js
+++ frontend/src/components/Wall.js
@@ -1,15 +1,42 @@
+import { useState, useEffect } from "react";
 import "../styles/Wall.css";
 
-//////////////////////////////////////////////////
-// - "Authorization": `Bearer ${window.sessionStorage.getItem("token")}`
-//////////////////////////////////////////////////
-
 function Wall() {
-  return fetch("http://localhost:3000/api/posts")
-    .then((res) => {
-      return res.json();
-    })
-    .catch((error) => alert("Erreur : " + error));
+  const [posts, setPosts] = useState([]);
+
+  async function allPosts() {
+    const res = await fetch("http://localhost:3000/api/posts", {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${JSON.parse(sessionStorage.getItem("token"))}`,
+      },
+    });
+    return await res.json();
+  }
+
+  useEffect(() => {
+    let mounted = true;
+    allPosts().then((items) => {
+      if (mounted) {
+        setPosts(items);
+      }
+    });
+    return () => (mounted = false);
+  }, []);
+
+  return (
+    <div>
+      <h1>Voici les derniers posts :</h1>
+      <ul>
+        {posts.map((item) => (
+          <li key={item.id}>
+            {item.text}
+            <img src={item.image} alt={item.image}></img>{" "}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Wall;
